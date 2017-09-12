d7be0006a2121a8dd82c612ea13e71a4325dfd0f
mxc415804388 <ysm19910225>
2017-09-12 15:04:56 +0800

    首页展示和测试分页

---
+++ XJDream-manager-web/src/main/java/com/XJDream/controller/PageController.java
@@ -0,0 +1,24 @@
+package com.XJDream.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * Created by Administrator on 2017/9/12.
+ * 展示页面首页
+ */
+@Controller
+public class PageController {
+
+    @RequestMapping("/")
+    public String showIndex(){
+        return "index";
+    }
+
+    @RequestMapping("/{page}")
+    public String showPage(@PathVariable String page){
+        return page;
+    }
+
+}
---
+++ XJDream-manager-web/src/test/com/XJDream/pageTest/TestPageHelper.java
@@ -0,0 +1,37 @@
+package com.XJDream.pageTest;
+
+import com.XJDream.mapper.TbItemMapper;
+import com.XJDream.pojo.TbItem;
+import com.XJDream.pojo.TbItemExample;
+import com.github.pagehelper.PageHelper;
+import com.github.pagehelper.PageInfo;
+import org.junit.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import java.util.List;
+
+/**
+ * Created by Administrator on 2017/9/12.
+ */
+public class TestPageHelper {
+    @Test
+    public void testPageHelper() throws Exception {
+        //1、获得mapper代理对象
+        ApplicationContext applicationContext = new ClassPathXmlApplicationContext("classpath:spring/applicationContext-*.xml");
+        TbItemMapper itemMapper = applicationContext.getBean(TbItemMapper.class);
+        //2、设置分页
+        PageHelper.startPage(1, 30);
+        //3、执行查询
+        TbItemExample example = new TbItemExample();
+        List<TbItem> list = itemMapper.selectByExample(example);
+        //4、取分页后结果
+        PageInfo<TbItem> pageInfo = new PageInfo<>(list);
+        long total = pageInfo.getTotal();
+        System.out.println("total:" + total);
+        int pages = pageInfo.getPages();
+        System.out.println("pages:" + pages);
+        int pageSize = pageInfo.getPageSize();
+        System.out.println("pageSize:" + pageSize);
+    }
+}
